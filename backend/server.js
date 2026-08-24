23c9d0c2d15df6acd6e9179d90e5bfac48c468db
Godfred ofori amanfoh <oforiamanfohgodfred@gmail.com>
2026-08-24 23:01:50 +0000

    feat: register classes API

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,20 @@ const organizationRoutes = require("./routes/organizationRoutes");
 const userRoutes = require("./routes/userRoutes");
 const teacherRoutes = require("./routes/teacherRoutes");
 const studentRoutes = require("./routes/studentRoutes");
+const classRoutes = require("./routes/classRoutes");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/organizations", organizationRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/teachers", teacherRoutes);
 app.use("/api/students", studentRoutes);
+app.use("/api/classes", classRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("🚀 EduSphere Backend is Running...");
 });
@@ -31,7 +30,6 @@ app.get("/test", (req, res) => {
   res.send("Test route works!");
 });
 
-// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
