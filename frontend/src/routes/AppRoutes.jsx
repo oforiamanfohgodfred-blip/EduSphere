f5b90a3ed1b7f29579bc0353f866f0de672ca356
Godfred ofori amanfoh <oforiamanfohgodfred@gmail.com>
2026-08-25 23:47:51 +0000

    feat: route student class learning space

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -12,6 +12,7 @@ import Resources from "../pages/teacher/Resources";
 import Announcements from "../pages/teacher/Announcements";
 import Profile from "../pages/teacher/Profile";
 import StudentDashboard from "../pages/student/Dashboard";
+import StudentClassSpace from "../pages/student/ClassSpace";
 import StudentAssignments from "../pages/student/Assignments";
 import StudentTimetables from "../pages/student/Timetables";
 import StudentExamNotifications from "../pages/student/ExamNotifications";
@@ -26,10 +27,45 @@ import ClassDetails from "../pages/organization/ClassDetails";
 import Subjects from "../pages/organization/Subjects";
 import Settings from "../pages/organization/Settings";
 import ProtectedRoute from "../components/ProtectedRoute";
-const p=(role,element)=><ProtectedRoute role={role}>{element}</ProtectedRoute>;
-function AppRoutes(){return <Routes>
-<Route path="/" element={<Login/>}/><Route path="/register" element={<RegisterChoice/>}/><Route path="/register-organization" element={<RegisterOrganization/>}/><Route path="/join-organization" element={<JoinOrganization/>}/><Route path="/organization-success" element={<OrganizationSuccess/>}/>
-<Route path="/teacher/dashboard" element={p("teacher",<TeacherDashboard/>)}/><Route path="/teacher/assignments" element={p("teacher",<Assignments/>)}/><Route path="/teacher/timetables" element={p("teacher",<Timetables/>)}/><Route path="/teacher/exams" element={p("teacher",<ExamNotifications/>)}/><Route path="/teacher/resources" element={p("teacher",<Resources/>)}/><Route path="/teacher/announcements" element={p("teacher",<Announcements/>)}/><Route path="/teacher/profile" element={p("teacher",<Profile/>)}/>
-<Route path="/student/dashboard" element={p("student",<StudentDashboard/>)}/><Route path="/student/assignments" element={p("student",<StudentAssignments/>)}/><Route path="/student/timetables" element={p("student",<StudentTimetables/>)}/><Route path="/student/exams" element={p("student",<StudentExamNotifications/>)}/><Route path="/student/resources" element={p("student",<StudentResources/>)}/><Route path="/student/announcements" element={p("student",<StudentAnnouncements/>)}/><Route path="/student/profile" element={p("student",<StudentProfile/>)}/>
-<Route path="/organization/dashboard" element={<OrganizationDashboard/>}/><Route path="/organization/teachers" element={<Teachers/>}/><Route path="/organization/students" element={<Students/>}/><Route path="/organization/classes" element={<Classes/>}/><Route path="/organization/classes/:id" element={<ClassDetails/>}/><Route path="/organization/subjects" element={<Subjects/>}/><Route path="/organization/settings" element={<Settings/>}/><Route path="*" element={<Navigate to="/"/>}/></Routes>}
+
+const p = (role, element) => <ProtectedRoute role={role}>{element}</ProtectedRoute>;
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<RegisterChoice />} />
+      <Route path="/register-organization" element={<RegisterOrganization />} />
+      <Route path="/join-organization" element={<JoinOrganization />} />
+      <Route path="/organization-success" element={<OrganizationSuccess />} />
+
+      <Route path="/teacher/dashboard" element={p("teacher", <TeacherDashboard />)} />
+      <Route path="/teacher/assignments" element={p("teacher", <Assignments />)} />
+      <Route path="/teacher/timetables" element={p("teacher", <Timetables />)} />
+      <Route path="/teacher/exams" element={p("teacher", <ExamNotifications />)} />
+      <Route path="/teacher/resources" element={p("teacher", <Resources />)} />
+      <Route path="/teacher/announcements" element={p("teacher", <Announcements />)} />
+      <Route path="/teacher/profile" element={p("teacher", <Profile />)} />
+
+      <Route path="/student/dashboard" element={p("student", <StudentDashboard />)} />
+      <Route path="/student/class" element={p("student", <StudentClassSpace />)} />
+      <Route path="/student/assignments" element={p("student", <StudentAssignments />)} />
+      <Route path="/student/timetables" element={p("student", <StudentTimetables />)} />
+      <Route path="/student/exams" element={p("student", <StudentExamNotifications />)} />
+      <Route path="/student/resources" element={p("student", <StudentResources />)} />
+      <Route path="/student/announcements" element={p("student", <StudentAnnouncements />)} />
+      <Route path="/student/profile" element={p("student", <StudentProfile />)} />
+
+      <Route path="/organization/dashboard" element={<OrganizationDashboard />} />
+      <Route path="/organization/teachers" element={<Teachers />} />
+      <Route path="/organization/students" element={<Students />} />
+      <Route path="/organization/classes" element={<Classes />} />
+      <Route path="/organization/classes/:id" element={<ClassDetails />} />
+      <Route path="/organization/subjects" element={<Subjects />} />
+      <Route path="/organization/settings" element={<Settings />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+}
+
 export default AppRoutes;
