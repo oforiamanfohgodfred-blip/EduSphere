b36b5662c7365f94443ac52ac85177adfb654d39
Godfred ofori amanfoh <oforiamanfohgodfred@gmail.com>
2026-09-06 21:30:49 +0000

    feat: route student class workspace

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -13,6 +13,7 @@ import Resources from "../pages/teacher/Resources";
 import Announcements from "../pages/teacher/Announcements";
 import Profile from "../pages/teacher/Profile";
 import StudentDashboard from "../pages/student/Dashboard";
+import StudentClassWorkspace from "../pages/student/ClassWorkspace";
 import StudentAssignments from "../pages/student/Assignments";
 import StudentTimetables from "../pages/student/Timetables";
 import StudentExamNotifications from "../pages/student/ExamNotifications";
@@ -31,42 +32,36 @@ import ProtectedRoute from "../components/ProtectedRoute";
 const protectedPage = (role, element) => <ProtectedRoute role={role}>{element}</ProtectedRoute>;
 
 function AppRoutes() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<RegisterChoice />} />
-      <Route path="/register-organization" element={<RegisterOrganization />} />
-      <Route path="/join-organization" element={<JoinOrganization />} />
-      <Route path="/organization-success" element={<OrganizationSuccess />} />
-
-      <Route path="/teacher/dashboard" element={protectedPage("teacher", <TeacherDashboard />)} />
-      <Route path="/teacher/classes/:id" element={protectedPage("teacher", <TeacherClassWorkspace />)} />
-      <Route path="/teacher/assignments" element={protectedPage("teacher", <Assignments />)} />
-      <Route path="/teacher/timetables" element={protectedPage("teacher", <Timetables />)} />
-      <Route path="/teacher/exams" element={protectedPage("teacher", <ExamNotifications />)} />
-      <Route path="/teacher/resources" element={protectedPage("teacher", <Resources />)} />
-      <Route path="/teacher/announcements" element={protectedPage("teacher", <Announcements />)} />
-      <Route path="/teacher/profile" element={protectedPage("teacher", <Profile />)} />
-
-      <Route path="/student/dashboard" element={protectedPage("student", <StudentDashboard />)} />
-      <Route path="/student/assignments" element={protectedPage("student", <StudentAssignments />)} />
-      <Route path="/student/timetables" element={protectedPage("student", <StudentTimetables />)} />
-      <Route path="/student/exams" element={protectedPage("student", <StudentExamNotifications />)} />
-      <Route path="/student/announcements" element={protectedPage("student", <StudentAnnouncements />)} />
-      <Route path="/student/resources" element={protectedPage("student", <StudentResources />)} />
-      <Route path="/student/profile" element={protectedPage("student", <StudentProfile />)} />
-
-      <Route path="/organization/dashboard" element={protectedPage("organization", <OrganizationDashboard />)} />
-      <Route path="/organization/teachers" element={protectedPage("organization", <Teachers />)} />
-      <Route path="/organization/students" element={protectedPage("organization", <Students />)} />
-      <Route path="/organization/classes" element={protectedPage("organization", <Classes />)} />
-      <Route path="/organization/classes/:id" element={protectedPage("organization", <ClassDetails />)} />
-      <Route path="/organization/subjects" element={protectedPage("organization", <Subjects />)} />
-      <Route path="/organization/settings" element={protectedPage("organization", <Settings />)} />
-
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="/register" element={<RegisterChoice />} />
+    <Route path="/register-organization" element={<RegisterOrganization />} />
+    <Route path="/join-organization" element={<JoinOrganization />} />
+    <Route path="/organization-success" element={<OrganizationSuccess />} />
+    <Route path="/teacher/dashboard" element={protectedPage("teacher", <TeacherDashboard />)} />
+    <Route path="/teacher/classes/:id" element={protectedPage("teacher", <TeacherClassWorkspace />)} />
+    <Route path="/teacher/assignments" element={protectedPage("teacher", <Assignments />)} />
+    <Route path="/teacher/timetables" element={protectedPage("teacher", <Timetables />)} />
+    <Route path="/teacher/exams" element={protectedPage("teacher", <ExamNotifications />)} />
+    <Route path="/teacher/resources" element={protectedPage("teacher", <Resources />)} />
+    <Route path="/teacher/announcements" element={protectedPage("teacher", <Announcements />)} />
+    <Route path="/teacher/profile" element={protectedPage("teacher", <Profile />)} />
+    <Route path="/student/dashboard" element={protectedPage("student", <StudentDashboard />)} />
+    <Route path="/student/classes/:id" element={protectedPage("student", <StudentClassWorkspace />)} />
+    <Route path="/student/assignments" element={protectedPage("student", <StudentAssignments />)} />
+    <Route path="/student/timetables" element={protectedPage("student", <StudentTimetables />)} />
+    <Route path="/student/exams" element={protectedPage("student", <StudentExamNotifications />)} />
+    <Route path="/student/announcements" element={protectedPage("student", <StudentAnnouncements />)} />
+    <Route path="/student/resources" element={protectedPage("student", <StudentResources />)} />
+    <Route path="/student/profile" element={protectedPage("student", <StudentProfile />)} />
+    <Route path="/organization/dashboard" element={protectedPage("organization", <OrganizationDashboard />)} />
+    <Route path="/organization/teachers" element={protectedPage("organization", <Teachers />)} />
+    <Route path="/organization/students" element={protectedPage("organization", <Students />)} />
+    <Route path="/organization/classes" element={protectedPage("organization", <Classes />)} />
+    <Route path="/organization/classes/:id" element={protectedPage("organization", <ClassDetails />)} />
+    <Route path="/organization/subjects" element={protectedPage("organization", <Subjects />)} />
+    <Route path="/organization/settings" element={protectedPage("organization", <Settings />)} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes>;
 }
-
 export default AppRoutes;
